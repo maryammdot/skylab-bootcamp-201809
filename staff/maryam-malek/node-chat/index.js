b6c3d26646e006a3864e9bf9e0a42f5bffd2d321
maryamdot <maryamdot@maryamdots-MacBook-Pro-2.local>
2018-10-30 22:20:56 +0100

    still not working

--- staff/maryam-malek/node-chat/index.js
+++ staff/maryam-malek/node-chat/index.js
@@ -6,22 +6,49 @@ let answerStr = ''
 
 var server = net.createServer(socket => {  
 
+    // socket.write('hello\n')
+    // socket.on('data', (err, data) => {
+    //   if(err) throw err
+    //   socket.write(data)
+    //   process.stdout.write(message)
+
+    // })
+
+    waitAnsw(socket)
+    socket.on('end', err =>{
+      if(err) throw err
+      socket.write('Bye')
+      process.stdout.write('Bye')
+    })
+    
+  })  
+  server.listen(port) 
+
+  function waitAnsw(socket){
     socket.write('hello\n')
+    socket.on('data', (err, data) => {
+      if(err) throw err
+      if(data !== 'end'){
+        socket.write(data)
+        process.stdout.write(data)
+        waitAnsw(socket)
+      }
 
-    const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout
-      });
-      
-      rl.question('Hello, how are you?', (answer) => {
-        answerStr = answer
-
-        console.log(`Thank you for your valuable feedback: ${answerStr}`);
-      
-        rl.close();
-      });
-
-    // socket.end('bye\n')
-
-})  
-server.listen(port) 
+    })
+    
+  }
+  
+      // const rl = readline.createInterface({
+      //     input: process.stdin,
+      //     output: process.stdout
+      //   });
+        
+      //   rl.question('Hello, how are you?', (answer) => {
+      //     answerStr = answer
+  
+      //     console.log(`Thank you for your valuable feedback: ${answerStr}`);
+        
+      //     rl.close();
+      //   });
+  
+      // // socket.end('bye\n')
