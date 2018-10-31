d7fe81e93c5d3ed3c74c5e27630ffb39edc40f37
maryamdot <maryamdot@maryamdots-MacBook-Pro-2.local>
2018-10-31 09:24:44 +0100

    chat done

--- staff/maryam-malek/node-chat/index.js
+++ staff/maryam-malek/node-chat/index.js
@@ -1,54 +1,13 @@
 var net = require('net')  
-const readline = require('readline');
 
 const [,, port] = process.argv
-let answerStr = ''
 
 var server = net.createServer(socket => {  
-
-    // socket.write('hello\n')
-    // socket.on('data', (err, data) => {
-    //   if(err) throw err
-    //   socket.write(data)
-    //   process.stdout.write(message)
-
-    // })
-
-    waitAnsw(socket)
-    socket.on('end', err =>{
-      if(err) throw err
-      socket.write('Bye')
-      process.stdout.write('Bye')
-    })
     
-  })  
-  server.listen(port) 
+  socket.on('data', data => process.stdout.write(data))
+  
+  process.stdin.on('data', data => socket.write(data))
+  
+})  
 
-  function waitAnsw(socket){
-    socket.write('hello\n')
-    socket.on('data', (err, data) => {
-      if(err) throw err
-      if(data !== 'end'){
-        socket.write(data)
-        process.stdout.write(data)
-        waitAnsw(socket)
-      }
-
-    })
-    
-  }
-  
-      // const rl = readline.createInterface({
-      //     input: process.stdin,
-      //     output: process.stdout
-      //   });
-        
-      //   rl.question('Hello, how are you?', (answer) => {
-      //     answerStr = answer
-  
-      //     console.log(`Thank you for your valuable feedback: ${answerStr}`);
-        
-      //     rl.close();
-      //   });
-  
-      // // socket.end('bye\n')
+server.listen(port) 
