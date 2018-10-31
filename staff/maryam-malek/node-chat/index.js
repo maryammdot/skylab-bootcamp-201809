d3679d3f6beb481521a76c3855d6776137357237
maryamdot <maryamdot@maryamdots-MacBook-Pro-2.local>
2018-10-31 11:33:47 +0100

    trying to print in more than 1 client

--- staff/maryam-malek/node-chat/index.js
+++ staff/maryam-malek/node-chat/index.js
@@ -1,13 +1,23 @@
-var net = require('net')  
+var net = require('net')
 
-const [,, port] = process.argv
+const [, , port] = process.argv
 
-var server = net.createServer(socket => {  
-    
-  socket.on('data', data => process.stdout.write(data))
+let clients = []
+
+var server = net.createServer(socket => {
+  clients.push(socket)
   
+  clients.forEach(client => {
+
+    client.on('data', data => {
+      process.stdout.write(data)
+
+
+    })
+
+  })
   process.stdin.on('data', data => socket.write(data))
-  
-})  
+
+})
 
 server.listen(port) 
